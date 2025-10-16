ad364433541b62a01e13bc818b90090f3413d6b9
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-16 10:06:11 +0900

    Implement verifier failure test in verifier_fail.bpf.c
    Add a BPF program that triggers verifier failure.

--- scripts/verifier_fail.bpf.c
+++ scripts/verifier_fail.bpf.c
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
 
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+/* verifier 실패 유발 테ㅛㅡ트 */
+SEC("tracepoint/syscalls/sys_enter_execve")
+int bad_prog(void *ctx)
+{
+    __u64 i = 0;
+    for (;;) {
+        i++;
+        if (i == 0xFFFFFFFFFFFFFFFFULL) {
+            break;
+        }
+    }
+    return 0;
+}
